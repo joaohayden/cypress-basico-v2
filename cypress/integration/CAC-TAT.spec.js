f22f3bd33f772fc3d83377b6fbc5d1b7a5f31c3c
joaohayden <joaohayden@gmail.com>
2025-01-03 16:10:53 -0400

    delay added on exercise extra 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -6,17 +6,19 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.visit('./src/index.html')
     })
 
-    it('verifica o título da aplicação',function(){
+    it('verifica o título da aplicação',function() {
         cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
     })
-    it('preenche os campos obrigatórios e envia o formulário', function(){
+    it('preenche os campos obrigatórios e envia o formulário', function() {
         cy.get('#firstName').type("João")
         cy.get('#lastName').type("Hayden")
         cy.get('#email').type("joaohayden@gmail.com")
         cy.get('#phone').type("92999999999")
-        cy.get('#open-text-area').type("Me ajude a aprender Cypress em uma semana")
-        cy.get('.button').click()
+        cy.get('#open-text-area').type("Lorem ipsum dolor sit amet. Et ipsam Quis vel quia recusandae et sapiente corporis ut officia iure. Aut necessitatibus tempora vel voluptatem ipsum ea omnis reiciendis ab quia sunt sit perferendis iste ut vero error et minima consequuntur. Qui dolores reiciendis ut rerum nesciunt rem alias voluptas eum voluptatem tempore eum nostrum Quis.", {delay: 0}) //tempo de digitação
+        cy.get('button[type="submit"]').click()
         cy.get('.success').should('be.visible')
     })
+    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+        
+    })
 })
-
