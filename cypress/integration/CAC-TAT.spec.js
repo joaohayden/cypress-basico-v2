d7dc1164214f6abf99a82fbfe79977ac252054c8
unknown <joaohayden@gmail.com>
2025-01-03 15:22:47 -0400

    exercicio1 de get

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,22 @@
-/// <reference types='cypress' />
+/// <reference types="cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function(){
+describe('Central de Atendimento ao Cliente TAT', function() {
+    
+    beforeEach(function() {
+        cy.visit('./src/index.html')
+    })
 
     it('verifica o título da aplicação',function(){
-        cy.visit('./src/index.html')
         cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+    })
+    it('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').type("João")
+        cy.get('#lastName').type("Hayden")
+        cy.get('#email').type("joaohayden@gmail.com")
+        cy.get('#phone').type("92999999999")
+        cy.get('#open-text-area').type("Me ajude a aprender Cypress em uma semana")
+        cy.get('.button').click()
+        cy.get('.success').should('be.visible')
     })
 })
 
