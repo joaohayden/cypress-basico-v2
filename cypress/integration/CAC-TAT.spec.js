2d68e14543f4b8906bc00c6900dcec22c8ea4474
joaohayden <joaohayden@gmail.com>
2025-01-04 23:44:37 -0400

    some updates to extra exercises until 7

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,15 +10,44 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
     })
     it('preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = 'Lorem ipsum dolor sit amet. Et ipsam Quis vel quia recusandae et sapiente corporis ut officia iure. Aut necessitatibus tempora vel voluptatem ipsum ea omnis reiciendis ab quia sunt sit perferendis iste ut vero error et minima consequuntur.' //adicionar const pra deixar o código menos mockado
         cy.get('#firstName').type("João")
         cy.get('#lastName').type("Hayden")
         cy.get('#email').type("joaohayden@gmail.com")
-        cy.get('#phone').type("92999999999")
-        cy.get('#open-text-area').type("Lorem ipsum dolor sit amet. Et ipsam Quis vel quia recusandae et sapiente corporis ut officia iure. Aut necessitatibus tempora vel voluptatem ipsum ea omnis reiciendis ab quia sunt sit perferendis iste ut vero error et minima consequuntur. Qui dolores reiciendis ut rerum nesciunt rem alias voluptas eum voluptatem tempore eum nostrum Quis.", {delay: 0}) //tempo de digitação
+        cy.get('#open-text-area').type(longText, {delay: 0})//tempo de digitação
         cy.get('button[type="submit"]').click()
         cy.get('.success').should('be.visible')
     })
-    it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
-        
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+
     })
+    
+    it('valida campo vazio ao tentar digitar valor não numérico no telefone', function(){
+        cy.get('#phone').type('Teste').should('have.value','') //se usar o not have pode ser mt trabalho -> usa o have.value e verifica se veio vazio
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+        cy.get('#phone-checkbox').click()
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+        const firstName = 'João'
+        const lastName = 'Hayden'
+        const email = 'joaohayden@gmail.com'
+        const phone = '92981020175'
+        cy.get('#firstName').type(firstName).should('have.value',firstName).clear().should('have.value','')
+        cy.get('#lastName').type(lastName).should('have.value',lastName).clear().should('have.value','')
+        cy.get('#email').type(email).should('have.value',email).clear().should('have.value','')
+        cy.get('#phone').type(phone).should('have.value',phone).clear().should('have.value','')
+    })
+
+    it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+    })
+
 })
